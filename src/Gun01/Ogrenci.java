0c06563ecf91e9cd7079603426acfbe7bb83c2fc
simalnb <sml.ylz.73@gmail.com>
2023-02-05 21:34:47 +0300

    gonder

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -3,7 +3,7 @@ package Gun01;
 public class Ogrenci {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            System.out.println("merhaba2");
+            System.out.println("merhaba3");
 
         }
 
